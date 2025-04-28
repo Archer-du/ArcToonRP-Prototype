a62f2adc9642a6340433152566cfc412ebcd7c9e
Archer-du <dupengche@gmail.com>
2025-04-28 17:44:18 +0800

    culling result for visible per object shadow casters

---
+++ Assets/ArcToon/Runtime/Utils/CullingResultsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ArcToon.Runtime.Utils
+{
+    public static class CullingResultsExtensions
+    {
+        // public static bool ComputePerObjectShadowMatricesAndCullingPrimitives(this CullingResults cullingResults,
+        //     int perObjectShadowCasterIndex,
+        //     int shadowedDirectionalLightIndex,
+        //     int shadowedDirectionalLightCount,
+        //     int shadowResolution,
+        //     float shadowNearPlaneOffset,
+        //     out Matrix4x4 viewMatrix,
+        //     out Matrix4x4 projMatrix,
+        //     out ShadowSplitData shadowSplitData)
+        // {
+        //     return false;
+        // }
+    }
+}
